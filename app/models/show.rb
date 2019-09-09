c2cd642b2d5249a93d033ba2b72e0ecf551614cb
Robinson Regalado <rregalado17@gmail.com>
2019-09-09 21:29:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,11 @@
 class Show < ActiveRecord::Base 
   
   def self.highest_rating 
-    self.maxium(:rating)
+    self.maximum(:rating)
+  end
+  
+  def self.most_popular_show 
+    self.where("rating = ?", self.highest_rating).first
   end
   
 end
